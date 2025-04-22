4df91cdadf4228d59ac06101d2028c4f753cde87
CuongLiam <cuongdeptrai2706@gmail.com>
2025-04-23 00:12:22 +0700

    fix

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,18 @@ public class Main {
         System.out.println("Nhập tháng: ");
         int months = scanner.nextInt();
 
-        if(months < 0 || months > 13){
+        if(months < 1 || months > 12){
             System.out.println("Tháng không hợp lệ");
             return;
         }
 
-        if (months == 2){
+        if (months == 2) {
             System.out.printf("Tháng %d có 28 ngày", months);
+        } else if (months == 1 || months == 3 || months == 5 || months == 7 || months == 8 || months == 10 || months == 12) {
+            System.out.printf("Tháng %d có 31 ngày", months);
+        } else {
+            System.out.printf("Tháng %d có 30 ngày", months);
         }
 
-        if(months == 1 || months == 3 || months == 5 || months == 7 || months == 8 || months == 10 || months == 12){
-            System.out.printf("tháng %d có 31 ngày", months);
-        }
-
-        if(months == 4 || months == 6 || months == 9 || months == 11){
-            System.out.printf("tháng %d có 30 ngày", months);
-        }
     }
 }
