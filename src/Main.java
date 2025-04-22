f102ae7898f822d6012873d6c68001088c24b480
CuongLiam <cuongdeptrai2706@gmail.com>
2025-04-22 23:55:59 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Nhập tháng: ");
+        int months = scanner.nextInt();
+
+        if(months < 0 || months > 13){
+            System.out.println("Tháng không hợp lệ");
+            return;
+        }
+
+        if (months == 2){
+            System.out.printf("Tháng %d có 28 ngày", months);
+        }
+
+        if(months == 1 || months == 3 || months == 5 || months == 7 || months == 8 || months == 10 || months == 12){
+            System.out.printf("tháng %d có 31 ngày", months);
+        }
+
+        if(months == 4 || months == 6 || months == 9 || months == 11){
+            System.out.printf("tháng %d có 30 ngày", months);
         }
     }
 }
